a63371f3658db80026dd7ff1661581b5d427a4ce
Dawid Zieliński <dawidson174@gmail.com>
2023-06-13 19:04:46 +0200

    improve new texture, repair texture, coin counter, add animation

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include <random>
+#include <vector>
 
 class Entity {
 public:
@@ -40,25 +41,69 @@ public:
         : Entity(texturePath, position) {}
 
     void update(float deltaTime) override {
-        float speed = 100.0f;
+        float speed = 150.0f;
         sf::Vector2f movement(0.0f, 0.0f);
+        bool isMovingright = false;
+        bool isMovingleft = false;
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
             movement.y -= speed;
+            isMovingright = true;
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
             movement.y += speed;
+            isMovingright = true;
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
             movement.x -= speed;
+            isMovingleft = true;
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
             movement.x += speed;
+            isMovingright = true;
         }
 
         position += movement * deltaTime;
         sprite.setPosition(position);
+
+        if (isMovingright) {
+            // Update animation frame
+            currentFrameTime += deltaTime;
+            if (currentFrameTime >= frameDuration) {
+                currentFrame++;
+                if (currentFrame >= animationFrames.size()) {
+                    currentFrame = 0;
+                }
+                currentFrameTime = 0.0f;
+            }
+            sprite.setTextureRect(animationFrames[currentFrame]);
+        }
+        else if (isMovingleft) {
+            // Update animation frame
+            currentFrameTime += deltaTime;
+            if (currentFrameTime >= frameDuration) {
+                currentFrame++;
+                if (currentFrame >= animationFramesleft.size()) {
+                    currentFrame = 0;
+                }
+                currentFrameTime = 0.0f;
+            }
+            sprite.setTextureRect(animationFramesleft[currentFrame]);
+        }
+        else {
+            // Player is idle
+            currentFrameTime += deltaTime;
+            if (currentFrameTime >= frameDuration) {
+                currentFrame++;
+                if (currentFrame >= idleFrames.size()) {
+                    currentFrame = 0;
+                }
+                currentFrameTime = 0.0f;
+            }
+            sprite.setTextureRect(idleFrames[currentFrame]);
+        }
     }
+
 
     bool hasKnife() const {
         return hasKnifeFlag;
@@ -68,14 +113,34 @@ public:
         hasKnifeFlag = hasKnife;
     }
 
+    void addAnimationFrame(const sf::IntRect& frame) {
+        animationFrames.push_back(frame);
+    }
+
+    void addIdleFrame(const sf::IntRect& frame) {
+        idleFrames.push_back(frame);
+    }
+
+    void addAnimationFrameleft(const sf::IntRect& frame) {
+        animationFramesleft.push_back(frame);
+    }
+
 private:
     bool hasKnifeFlag = false;
+    std::vector<sf::IntRect> animationFrames;
+    std::vector<sf::IntRect> idleFrames;
+    std::vector<sf::IntRect> animationFramesleft;
+    unsigned int currentFrame = 0;
+    float currentFrameTime = 0.0f;
+    float frameDuration = 0.2f;
 };
 
 class Monster : public Entity {
 public:
     Monster(const std::string& texturePath, const sf::Vector2f& position, float speed)
-        : Entity(texturePath, position), speed(speed) {}
+        : Entity(texturePath, position), speed(speed) {
+        sprite.setScale(0.2f, 0.2f);
+    }
 
     void update(float deltaTime) override {
         // Move towards the player
@@ -137,6 +202,26 @@ public:
         // Create player
         player = std::make_unique<Player>("player.png", sf::Vector2f(400.0f, 300.0f));
 
+        player->sprite.setScale(2.5f, 2.5f);
+
+        player->addIdleFrame(sf::IntRect(0, 0, 50, 50));
+        player->addIdleFrame(sf::IntRect(50, 0, 50, 50));
+        player->addIdleFrame(sf::IntRect(100, 0, 50, 50));
+
+        player->addAnimationFrame(sf::IntRect(150, 0, 50, 50));
+        player->addAnimationFrame(sf::IntRect(200, 0, 50, 50));
+        player->addAnimationFrame(sf::IntRect(250, 0, 50, 50));
+        player->addAnimationFrame(sf::IntRect(300, 0, 50, 50));
+        player->addAnimationFrame(sf::IntRect(350, 0, 50, 50));
+        player->addAnimationFrame(sf::IntRect(400, 0, 50, 50));
+
+        player->addAnimationFrameleft(sf::IntRect(450, 0, 50, 50));
+        player->addAnimationFrameleft(sf::IntRect(500, 0, 50, 50));
+        player->addAnimationFrameleft(sf::IntRect(550, 0, 50, 50));
+        player->addAnimationFrameleft(sf::IntRect(600, 0, 50, 50));
+        player->addAnimationFrameleft(sf::IntRect(650, 0, 50, 50));
+        player->addAnimationFrameleft(sf::IntRect(700, 0, 50, 50));
+
         // Create monsters
         for (int i = 0; i < 3; ++i) {
             float xPos = static_cast<float>(rand() % 600 + 100);
@@ -145,18 +230,44 @@ public:
         }
 
         // Create obstacles
-        obstacles.push_back(std::make_unique<Obstacle>("obstacle.png", sf::Vector2f(100.0f, 200.0f), sf::Vector2f(200.0f, 100.0f)));
-        obstacles.push_back(std::make_unique<Obstacle>("obstacle.png", sf::Vector2f(200.0f, 100.0f), sf::Vector2f(500.0f, 400.0f)));
+        obstacles.push_back(std::make_unique<Obstacle>("Sciana800.png", sf::Vector2f(800, 40), sf::Vector2f(0, 0)));
+        obstacles.push_back(std::make_unique<Obstacle>("Sciana800dol.png", sf::Vector2f(800, 40), sf::Vector2f(0, 560)));
+        obstacles.push_back(std::make_unique<Obstacle>("Sciana40x600.png", sf::Vector2f(40, 600), sf::Vector2f(0, 0)));
+        obstacles.push_back(std::make_unique<Obstacle>("Sciana40x600prawo.png", sf::Vector2f(40, 600), sf::Vector2f(760, 0)));
+        obstacles.push_back(std::make_unique<Obstacle>("floor.png", sf::Vector2f(720, 520), sf::Vector2f(40, 40)));
 
         // Create coins
         coins.push_back(std::make_unique<Coin>(sf::Vector2f(300.0f, 200.0f)));
         coins.push_back(std::make_unique<Coin>(sf::Vector2f(600.0f, 400.0f)));
+
+        // Load font
+        if (!font.loadFromFile("arial.ttf")) {
+            std::cerr << "Error loading font: arial.ttf" << std::endl;
+        }
+
+        // Create coin count text
+        coinCountText.setFont(font);
+        coinCountText.setCharacterSize(20);
+        coinCountText.setFillColor(sf::Color::White);
+        coinCountText.setPosition(680.f, 10.f);
 
         // Create knife
         knife = std::make_unique<Knife>(sf::Vector2f(100.0f, 100.0f));
     }
     void update(float deltaTime) {
         player->update(deltaTime);
+
+        // Count collected coins
+        int collectedCoinCount = 0;
+        for (const auto& coin : coins) {
+            if (coin->collected) {
+                collectedCoinCount++;
+            }
+        }
+
+        // Update coin count text
+        coinCountText.setString("Coins: " + std::to_string(collectedCoinCount));
+
 
         // Check collision between player and coins
         for (auto& coin : coins) {
@@ -184,7 +295,7 @@ public:
                     continue;
                 } else {
                     // Game over condition when player collides with a monster without the knife
-                    std::cout << "Game Over!" << std::endl;
+                    //std::cout << "Game Over!" << std::endl;
                     break;
                 }
             }
@@ -199,9 +310,6 @@ public:
         for (auto& coin : coins) {
             coin->update(deltaTime);
         }
-        sprite.setTexture(texture);
-        sprite.setPosition(position);
-        visible = false;
     }
     void run() {
         sf::Clock clock;
@@ -221,19 +329,9 @@ public:
         }
     }
 
-    void setVisibility(bool visible) {
-        this->visible = visible;
-    }
-
-    void addHeart() {
-        sf::Sprite heart;
-        heart.setTexture(texture);
-        float x = sprite.getPosition().x + (hearts.size() * sprite.getGlobalBounds().width);
-        float y = sprite.getPosition().y;
-        heart.setPosition(sf::Vector2f(x, y));
-        hearts.push_back(heart);
-    }
-
+    //   void setVisibility(bool visible) {
+    //       this->visible = visible;
+    //   }
 private:
     sf::RenderWindow window;
     std::unique_ptr<Player> player;
@@ -241,6 +339,8 @@ private:
     std::vector<std::unique_ptr<Obstacle>> obstacles;
     std::vector<std::unique_ptr<Coin>> coins;
     std::unique_ptr<Knife> knife;
+    sf::Font font;
+    sf::Text coinCountText;
 
     void update1(float deltaTime) {
         player->update(deltaTime);
@@ -287,12 +387,6 @@ private:
     void render() {
         window.clear();
 
-        window.draw(player->sprite);
-
-        for (auto& monster : monsters) {
-            window.draw(monster->sprite);
-        }
-
         for (auto& obstacle : obstacles) {
             window.draw(obstacle->sprite);
         }
@@ -307,12 +401,30 @@ private:
             window.draw(knife->sprite);
         }
 
+        for (auto& monster : monsters) {
+            window.draw(monster->sprite);
+        }
+
+        window.draw(player->sprite);
+
+        // Draw coin count text
+        window.draw(coinCountText);
+
         window.display();
     }
 };
 
 int main() {
     srand(static_cast<unsigned>(time(nullptr)));
+
+    // Load font
+    sf::Font font;
+    if (!font.loadFromFile("arial.ttf")) {
+        std::cerr << "Error loading font." << std::endl;
+        return 1;
+    }
+
+
 
     Game game;
     game.run();
